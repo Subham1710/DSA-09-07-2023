e3db80bb2672e8e6fffe776cb7da6033aa207b10
Bhadkaashiq <subhammishra@MacBook-Air-5.local>
2023-07-09 13:39:23 +0530

    first commit

---
+++ Linear_Search/leetcode.js
@@ -0,0 +1,22 @@
+// Linear search 
+
+const ele = x;
+const a = [];
+
+function linear(a,ele){
+
+   for (const i = 0; i< a.length; i++ )
+
+   if (a[i] == ele) {
+   
+    return i;
+
+   }
+
+   return null;
+   
+  
+
+}
+
+
